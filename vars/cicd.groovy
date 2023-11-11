9b1239d25e422197f17c5b00754328228eaefd1b
imranbasha0321 <149094832+imranbasha0321@users.noreply.github.com>
2023-11-11 13:33:29 +0000

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,1 +1,17 @@
-v
+     
+def gitDownload(repo)
+{ git "https://github.com/intelliqittrainings/${repo}.git"
+}
+def buildmaven()
+{
+  sh 'mvn package'
+}
+def tomcatdeploy(name,ip,context)
+{
+sh "scp /var/lib/jenkins/workspace/${name}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+}
+
+def runselinium(name)
+{
+sh "java -jar /var/lib/jenkins/workspace/${name}/testing.jar"
+}
